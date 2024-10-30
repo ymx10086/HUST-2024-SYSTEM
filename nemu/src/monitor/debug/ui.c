5aee44d9f29ef342872d947014af2cab7500047d
tracer-ics2019 <tracer@njuics.org>
2024-10-30 10:09:44 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:09:44 up 48 min,  0 users,  load average: 0.12, 0.09, 0.02 f8d015d14ecfb533384d1cdb5c7c8f4a5e14ce3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -72,13 +72,13 @@ static int cmd_x(char *args) {
   }
   int n = atoi(N);
   paddr_t addr = strtol(address, NULL, 16);
-  for(int i=0; i<n; i++) {
+  for(int i = 0; i < n; i++) {
       printf("0x%08x: ", addr);
-      for(int j=0; j<4; j++) {
-          printf("%02x ", paddr_read(addr, 1));
-          addr++;
-      }
-      printf("\n");
+      // for(int j=0; j<4; j++) {
+      //     printf("%02x ", paddr_read(addr, 1));
+      //     addr++;
+      // }
+      // printf("\n");
   }
   return 0;
 }
