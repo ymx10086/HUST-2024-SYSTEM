0b3355f9d2bd8abf86cb915643f26baf3b7501b6
tracer-ics2019 <tracer@njuics.org>
2024-10-30 09:39:47 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:39:47 up 18 min,  0 users,  load average: 0.02, 0.02, 0.00 4acc02b5693ff7e99f8cb71ea6870249ff20f6cb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -42,7 +42,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args){
-  char *pcs = strtok(NULL, " ");
+  char *pcs = strtok(args, " ");
   int n = 1;
   if(!pcs) n = atoi(pcs);
   cpu_exec(n);
