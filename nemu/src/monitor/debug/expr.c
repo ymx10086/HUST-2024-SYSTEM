11f9fd54a6ef893f57d36d15ce36aed3ebbd1abf
tracer-ics2019 <tracer@njuics.org>
2024-10-30 11:35:31 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:35:31 up  2:14,  0 users,  load average: 0.01, 0.02, 0.00 decbda1a4f0421fa200c76bce6c44a381959e8b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -157,6 +157,7 @@ static bool make_token(char *e) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
+    printf("one by one");
   }
 
   return true;
