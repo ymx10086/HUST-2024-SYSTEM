0dc5adfa7894e0d3f5b47fe0e86035a8ee64434a
tracer-ics2019 <tracer@njuics.org>
2024-11-06 16:56:49 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:56:49 up  7:05,  1 user,  load average: 0.30, 0.22, 0.09 c6c33515b9ea887a85f5bc24a21157e02e21b3d9

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,5 +14,11 @@ void isa_reg_display() {
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
+  for (int i = 0; i < 32; i++) {
+    if (strcmp(regsl[i], s) == 0) {
+      *success = true;
+      return cpu.gpr[i]._32;
+    }
+  }
   return 0;
 }
--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <regex.h>
 
+uint32_t isa_reg_str2val(const char *s, bool *success);
+
 enum {
   TK_NOTYPE = 256, TK_EQ,
 
