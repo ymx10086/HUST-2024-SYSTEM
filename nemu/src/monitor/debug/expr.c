beb2f970d79fe582ad88f9236f35f79e38aa5686
tracer-ics2019 <tracer@njuics.org>
2024-11-06 17:08:27 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:08:27 up  7:17,  1 user,  load average: 0.36, 0.20, 0.12 a08c8f4b1eb02b38b4b93d4744031ee31bb3c97

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -162,13 +162,7 @@ static bool make_token(char *e) {
     }
   }
 
-  printf("nr_token: %d\n", nr_token);
-  printf("token_str: %s\n", tokens[0].str);
-  printf("token_type: %d\n", tokens[0].type);
-  printf("token_str: %s\n", tokens[1].str);
-  printf("token_type: %d\n", tokens[1].type);
-  printf("token_str: %s\n", tokens[2].str);
-  printf("token_type: %d\n", tokens[2].type);
+  // printf("nr_token: %d\n", nr_token);
 
   return true;
 }
