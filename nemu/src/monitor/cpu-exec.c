57687ff3d877ee54884bcf19bfa0f13598a5ee69
tracer-ics2019 <tracer@njuics.org>
2024-11-10 20:15:24 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:15:24 up 1 day,  4:37,  0 users,  load average: 0.23, 0.08, 0.02 827a82f15eea40a2213a36f1a36d490a72c26ac4

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -37,7 +37,7 @@ void cpu_exec(uint64_t n) {
       return;
     default: nemu_state.state = NEMU_RUNNING;
   }
-  printf("N : %d\n", n);
+  printf("N : %ld\n", n);
 
   for (; n > 0; n --) {
     __attribute__((unused)) vaddr_t ori_pc = cpu.pc;
