f0244bd929f55f27a4dea11c2890334edbcc4c7f
tracer-ics2019 <tracer@njuics.org>
2024-11-10 20:16:45 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:16:45 up 1 day,  4:38,  0 users,  load average: 0.12, 0.08, 0.02 c3ead89b30fc1761855b98e84777b8d704281ca

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -40,7 +40,7 @@ void cpu_exec(uint64_t n) {
   printf("N : %ld\n", n);
 
   for (; n > 0; n --) {
-    printf("N : %ld\n", n > 0);
+    printf("N : %d\n", n > 0);
     __attribute__((unused)) vaddr_t ori_pc = cpu.pc;
 
     /* Execute one instruction, including instruction fetch,
