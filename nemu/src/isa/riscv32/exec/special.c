f88691ca8abf0fda805c14cb835dd90af4ff3c43
tracer-ics2019 <tracer@njuics.org>
2024-11-10 21:00:33 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:00:33 up 1 day,  5:22,  0 users,  load average: 0.40, 0.24, 0.14 5221516ffcc22571c9fd4e4ed96572d32c08eba8

--- nemu/src/isa/riscv32/exec/special.c
+++ nemu/src/isa/riscv32/exec/special.c
@@ -24,7 +24,7 @@ make_EHelper(nemu_trap) {
 
   rtl_exit(NEMU_END, cpu.pc, cpu.gpr[10]._32); // grp[10] is $a0
 
-  printf("nemu trap\n");
+  printf("nemu trap%d\n", nemu_state.state);
 
   print_asm("nemu trap");
   return;
