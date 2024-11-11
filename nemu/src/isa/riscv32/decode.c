271ffe046398d606c20c28a7144105a00db0507e
tracer-ics2019 <tracer@njuics.org>
2024-11-11 12:46:11 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:46:11 up 1 day, 21:08,  1 user,  load average: 0.03, 0.05, 0.01 420377f6d3d18b8ec69b48c298a1b14b2d579e5e

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -59,3 +59,14 @@ make_DHelper(st) {
 
   decode_op_r(id_dest, decinfo.isa.instr.rs2, true);
 }
+
+make_DHelper(J){
+  s0 = (decinfo.isa.instr.simm20 << 20) | (decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm11_ << 11) | (decinfo.isa.instr.imm10_1 << 1);   
+  s0 = (uint32_t)((int32_t)(s0 << 11) >> 11);
+  
+  decode_op_i(id_src, s0, true);
+  decode_op_r(id_dest, decinfo.isa.instr.rd, false);
+
+  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", id_src->val);
+  print_Dop(id_dest->str, OP_STR_SIZE, "0x%x", id_dest->reg);
+}
