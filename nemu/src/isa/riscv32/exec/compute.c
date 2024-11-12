7cb52ab3ef418981636b68a707270d6cb53851d3
tracer-ics2019 <tracer@njuics.org>
2024-11-12 10:53:52 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:53:52 up 2 days, 19:15,  0 users,  load average: 0.18, 0.21, 0.16 306d3592fe304e34b7a98d2347751724cb70c160

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -13,13 +13,57 @@ make_EHelper(auipc) {
   print_asm_template2(auipc);
 }
 
-make_EHelper(addi) {
-  rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
-  rtl_sr(id_dest->reg, &id_dest->val, 4);
+// make_EHelper(addi) {
+//   rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+//   rtl_sr(id_dest->reg, &id_dest->val, 4);
   
-  print_asm_template3(addi);
+//   print_asm_template3(addi);
+// }
+
+/**
+ * Execute the I-type instruction.
+*/
+make_EHelper(I_instr){
+  switch (decinfo.isa.instr.funct3) {
+    case 0x0: // addi
+      rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x1: // slli
+      rtl_shli(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x2: // slti
+      rtl_setrelop(RELOP_LT, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x3: // sltiu
+      rtl_setrelop(RELOP_LTU, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x4: // xori
+      rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x5: // srli/srai
+      if (decinfo.isa.instr.imm11 == 0) {
+        rtl_shri(&id_dest->val, &id_src->val, &id_src2->val);
+      } else {
+        rtl_sari(&id_dest->val, &id_src->val, &id_src2->val);
+      }
+      break;
+    case 0x6: // ori
+      rtl_or(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x7: // andi
+      rtl_and(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    default:
+      assert(0);
+  }
+  rtl_sr(id_dest->reg, &id_dest->val, 4);
+
+  print_asm_template3(I_instr);
 }
 
+/**
+ * Execute the J-type instruction.
+*/
 make_EHelper(jal) {
   rtl_sr(id_dest->reg, &decinfo.seq_pc, 4);
   rtl_j(decinfo.seq_pc + id_src->val);
