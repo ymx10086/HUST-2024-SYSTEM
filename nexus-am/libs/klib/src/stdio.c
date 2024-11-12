86f6bc19d18821ef0077871bbd1fd503b9aae68f
tracer-ics2019 <tracer@njuics.org>
2024-11-12 18:12:23 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:12:23 up 3 days,  2:34,  0 users,  load average: 0.21, 0.14, 0.13 752669d653a3e86d951c9132ab456ab5861610d9

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,108 +3,191 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define MAXN 256
+
+void add_string(char **s, char *str);
+void add_char(char **s, char c);
+void add_number(char **s, int num,char mode);
+void add_special_number(char **s, const char *fmt, va_list ap,char c);
 
 int printf(const char *fmt, ...) {
-  va_list vl;
-  va_start(vl, fmt);
-
-  char out[MAXN]; 
-  int length = vsprintf(out,fmt,vl);
-  for(int i = 0; i < length; i++) {
-    _putc(out[i]);
+  va_list ap;
+  va_start(ap,fmt);
+  char buf[1000];
+  int length=vsprintf(buf,fmt,ap);
+  buf[length]='\0';
+  for(int i=0;i<length;i++){
+    _putc(buf[i]);
   }
-  va_end(vl);
-
+  va_end(ap);
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  bool format = FALSE;
-  bool longarg = FALSE;
-  size_t pos = 0;
-
-  for (; *fmt; fmt++) {
-    if (format) {
-      switch (*fmt) {
-        case 'l':
-          longarg = TRUE;
-          break;
-        case 'p':
-          longarg = TRUE;
-          if (++pos < MAXN) out[pos - 1] = '0';
-          if (++pos < MAXN) out[pos - 1] = 'x';
-        case 'x': {
-          long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-          for (int i = 2 * (longarg ? sizeof(long) : sizeof(int)) - 1; i >= 0; i--) {
-            int d = (num >> (4 * i)) & 0xF;
-            if (++pos < MAXN) out[pos - 1] = (d < 10 ? '0' + d : 'a' + d - 10);
-          }
-          longarg = FALSE;
-          format = FALSE;
+  char *temp=out;
+  while(*fmt!='\0'){
+    if(*fmt=='%'){
+      fmt++;
+      switch(*fmt){
+        case 's':{
+          char *str=va_arg(ap,char*);
+          add_string(&temp,str);
           break;
         }
-        case 'd': {
-          long num = longarg ? va_arg(ap, long) : va_arg(ap, int);
-          if (num < 0) {
-            num = -num;
-            if (++pos < MAXN) out[pos - 1] = '-';
-          }
-          long digits = 1;
-          for (long nn = num; nn /= 10; digits++)
-            ;
-          for (int i = digits - 1; i >= 0; i--) {
-            if (pos + i + 1 < MAXN) out[pos + i] = '0' + (num % 10);
-            num /= 10;
-          }
-          pos += digits;
-          longarg = FALSE;
-          format = FALSE;
+        case 'd':{
+          int num=va_arg(ap,int);
+          add_number(&temp,num,'d');
           break;
         }
-        case 's': {
-          const char* s2 = va_arg(ap, const char*);
-          while (*s2) {
-            if (++pos < MAXN) out[pos - 1] = *s2;
-            s2++;
-          }
-          longarg = FALSE;
-          format = FALSE;
+        case 'x':{
+          int num=va_arg(ap,int);
+          add_number(&temp,num,'x');
           break;
         }
-        case 'c': {
-          if (++pos < MAXN) out[pos - 1] = (char)va_arg(ap, int);
-          longarg = FALSE;
-          format = FALSE;
+        case '0':{
+          fmt++;
+          add_special_number(&temp,fmt,ap,'0');
+          fmt++;
           break;
         }
-        default:
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':{
+          add_special_number(&temp,fmt,ap,' ');
+          fmt++;
           break;
+        }
+        case 'c':{
+          char c=va_arg(ap,int);
+          add_char(&temp,c);
+          break;
+        }
       }
-    } else if (*fmt == '%')
-      format = TRUE;
-    else if (++pos < MAXN)
-      out[pos - 1] = *fmt;
+      fmt++;
+    }
+    else{
+      *temp++=*fmt++;
+    }
   }
-  if (pos < MAXN)
-    out[pos] = 0;
-  else if (MAXN)
-    out[MAXN - 1] = 0;
-  return pos;
+  *temp='\0';
+  return temp-out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list vl;
-  va_start(vl, fmt);
-
-  int length = vsprintf(out,fmt,vl);
-
-  va_end(vl);
+  va_list ap;
+  va_start(ap,fmt);
+  int length=vsprintf(out,fmt,ap);
+  va_end(ap);
   return length;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
+}
+
+
+void add_string(char **s, char *str) {
+  while (*str!='\0') {
+    **s = *str;
+    (*s)++;
+    str++;
+  }
+}
+
+void add_char(char **s, char c) {
+  **s = c;
+  (*s)++;
+}
+
+void add_number(char **s, int num,char mode) {
+  if (num == 0) {
+    add_char(s, '0');
+    return;
+  }
+  char temp[100];
+  int i = 0;
+  if(num<0){
+    if(mode == 'd')
+      add_char(s,'-');
+    num=-num;
+  }
+  if(mode=='d'){
+    while (num) {
+      temp[i] = num % 10 + '0';
+      num /= 10;
+      i++;
+    }
+    while (i) {
+      add_char(s, temp[i - 1]);
+      i--;
+    }
+  }
+  else if(mode=='x'){
+    add_string(s,"0x");
+    while (num) {
+      temp[i] = num % 16 + (num % 16 < 10 ? '0' : 'a' - 10);
+      num /= 16;
+      i++;
+    }
+    while (i) {
+      add_char(s, temp[i - 1]);
+      i--;
+    }
+  }
+}
+
+
+void add_special_number(char **s, const char *fmt, va_list ap,char c) {
+  int num_of_digit=0;
+  while(*fmt!='d'&&*fmt!='x'){
+    num_of_digit=num_of_digit*10+*fmt-'0';
+    fmt++;
+  }
+  int num = va_arg(ap, int);
+  char temp[100];
+  if(*fmt=='d'){
+    int k = num==0?1:0;
+    if(num<0){
+      add_char(s,'-');
+      num=-num;
+    }
+    while (num) {
+      temp[k] = num % 10 + '0';
+      num /= 10;
+      k++;
+    }
+    int i=k;
+    while(i<num_of_digit){
+      add_char(s,c);
+      i++;
+    }
+    while(k){
+      add_char(s,temp[k-1]);
+      k--;
+    }
+  }
+  else if(*fmt=='x'){
+    int k = num==0?1:0;
+    while (num) {
+      temp[k] = num % 16 + (num % 16 < 10 ? '0' : 'a' - 10);
+      num /= 16;
+      k++;
+    }
+    int i=k;
+    while(i<num_of_digit){
+      add_char(s,c);
+      i++;
+    }
+    while(k){
+      add_char(s,temp[k-1]);
+      k--;
+    }
+  }
 }
 
 #endif
