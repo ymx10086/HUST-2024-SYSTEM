a170bda8cb5bfc94d6bc8943e946dbf4e35bd953
tracer-ics2019 <tracer@njuics.org>
2024-11-12 16:16:21 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:16:21 up 3 days, 38 min,  0 users,  load average: 0.14, 0.11, 0.04 51e6f6a5f0948cd09071822ad7e90f0fb6704490

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -27,38 +27,45 @@ make_EHelper(I_instr){
   switch (decinfo.isa.instr.funct3) {
     case 0x0: // addi
       rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(addi);
       break;
     case 0x1: // slli
       rtl_shl(&id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(slli);
       break;
     case 0x2: // slti
       rtl_setrelop(RELOP_LT, &id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(slti);
       break;
     case 0x3: // sltiu
       rtl_setrelop(RELOP_LTU, &id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(sltiu);
       break;
     case 0x4: // xori
       rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(xori);
       break;
     case 0x5: // srli/srai
       if (decinfo.isa.instr.imm11 == 0) {
         rtl_shr(&id_dest->val, &id_src->val, &id_src2->val);
+        print_asm_template3(srli);
       } else {
         rtl_sar(&id_dest->val, &id_src->val, &id_src2->val);
+        print_asm_template3(srai);
       }
       break;
     case 0x6: // ori
       rtl_or(&id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(ori);
       break;
     case 0x7: // andi
       rtl_and(&id_dest->val, &id_src->val, &id_src2->val);
+      print_asm_template3(andi);
       break;
     default:
       assert(0);
   }
   rtl_sr(id_dest->reg, &id_dest->val, 4);
-
-  print_asm_template3(I_instr);
 }
 
 /**
@@ -69,6 +76,66 @@ make_EHelper(jal) {
   rtl_j(decinfo.seq_pc + id_src->val);
 
   print_asm_template2(jal);
+}
+
+/**
+ * Execute the R-type instruction.
+*/
+make_EHelper(R){
+  switch (decinfo.isa.instr.funct3) {
+    case 0x0:
+      switch (decinfo.isa.instr.funct7){
+        case 0x0: // add
+          rtl_add(&id_dest->val, &id_src->val, &id_src2->val);
+          print_asm_template3(add);
+          break;
+        case 0x20: // sub
+          rtl_sub(&id_dest->val, &id_src->val, &id_src2->val);
+          print_asm_template3(sub);
+          break;
+        default: // mul
+          rtl_imul_lo(&id_dest->val, &id_src->val, &id_src2->val);
+          print_asm_template3(mul);
+          break;
+      }
+      break;
+    case 0x1: // sll
+      switch (decinfo.isa.instr.funct7){
+        case 0x0: // sll
+          rtl_shl(&id_dest->val, &id_src->val, &id_src2->val);
+          break;
+        default:
+          assert(0);
+      }
+      break;
+    case 0x2: // slt
+      rtl_setrelop(RELOP_LT, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x3: // sltu
+      rtl_setrelop(RELOP_LTU, &id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x4: // xor
+      rtl_xor(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x5: // srl/sra
+      if (decinfo.isa.instr.funct7 == 0) {
+        rtl_shr(&id_dest->val, &id_src->val, &id_src2->val);
+      } else {
+        rtl_sar(&id_dest->val, &id_src->val, &id_src2->val);
+      }
+      break;
+    case 0x6: // or
+      rtl_or(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    case 0x7: // and
+      rtl_and(&id_dest->val, &id_src->val, &id_src2->val);
+      break;
+    default:
+      assert(0);
+  }
+  rtl_sr(id_dest->reg, &id_dest->val, 4);
+
+  print_asm_template3(R);
 }
 
 make_EHelper(jalr){
