4b87a521eb70d8bdea9ab87f55263a152df71a9c
U202115514 <ymx@hust.edu.cn>
2024-11-13 09:57:38 +0800

    Ready

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -9,7 +9,7 @@ int screen_height();
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
-      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
+      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;v
       info->width = 400;
       info->height = 300;
       return sizeof(_DEV_VIDEO_INFO_t);
