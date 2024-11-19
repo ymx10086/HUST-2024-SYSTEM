7b47653841e7a6a6519de02182c18de37a8bfbc6
tracer-ics2019 <tracer@njuics.org>
2024-11-19 19:10:10 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:10:10 up  1:59,  0 users,  load average: 0.17, 0.14, 0.10 66abb76be74171471f6c10aa52b41a37f494dde9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,35 @@
 #include "common.h"
 #include "syscall.h"
 
+void sys_yield();
+void sys_exit(int code);
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
 
   switch (a[0]) {
+    case SYS_yield:
+      sys_yield();
+      c->GPRx = 0;
+      break;
+    case SYS_exit:
+      sys_exit(a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
+}
+
+void sys_yield() {
+  _yield();
+}
+
+void sys_exit(int code) {
+  _halt(code);
 }
--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -9,6 +9,7 @@ _Context* __am_irq_handle(_Context *c) {
     _Event ev = {0};
     switch (c->cause) {
       case -1: ev.event = _EVENT_YIELD; break;
+      case 0: case 1: ev.event = _EVENT_SYSCALL; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
