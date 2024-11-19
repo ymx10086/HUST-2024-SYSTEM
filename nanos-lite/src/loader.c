5152bccd6c98bcd13008dfe16ff3f4fb21293e45
tracer-ics2019 <tracer@njuics.org>
2024-11-19 18:22:34 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:22:34 up  1:11,  0 users,  load average: 0.27, 0.17, 0.11 9554f1319f6e43690c1bdcd722b5b280a66053

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,20 +10,9 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  // TODO();
-  Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
-
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; i++) {
-    if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
-    }
-  }
-  return ehdr.e_entry;
+  TODO();
+  
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
