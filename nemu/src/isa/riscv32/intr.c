4bce0b7f84bc7500e7e823caff728060bbc250ee
tracer-ics2019 <tracer@njuics.org>
2024-11-19 18:51:23 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:51:23 up  1:40,  0 users,  load average: 0.21, 0.15, 0.10 a3efe61083817d8ea9c474eaddb94e72502c5b54

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -1,17 +1,10 @@
 #include "rtl/rtl.h"
 #include "cpu/exec.h"
 
-// void raise_intr(uint32_t NO, vaddr_t epc) {
-//   /* TODO: Trigger an interrupt/exception with ``NO''.
-//    * That is, use ``NO'' to index the IDT.
-//    */
-//   decinfo.isa.sepc = epc;
-//   decinfo.isa.scause = NO;
-//   decinfo.jmp_pc = decinfo.isa.stvec;
-//   rtl_j(decinfo.jmp_pc);
-// }
-
 void raise_intr(uint32_t NO, vaddr_t epc) {
+  /* TODO: Trigger an interrupt/exception with ``NO''.
+   * That is, use ``NO'' to index the IDT.
+   */
   decinfo.isa.sepc = epc;
   decinfo.isa.scause = NO;
   decinfo.jmp_pc = decinfo.isa.stvec;
