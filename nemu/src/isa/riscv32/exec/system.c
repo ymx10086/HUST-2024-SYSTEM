9f465db5f45ad74261562c30e37a30a830b7802a
tracer-ics2019 <tracer@njuics.org>
2024-11-19 18:12:04 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:12:04 up  1:00,  0 users,  load average: 0.09, 0.13, 0.09 5e6e6239ba8852a883a26186404e7eec33c7baa

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -2,69 +2,54 @@
 
 extern void raise_intr(uint32_t NO, vaddr_t epc);
 
-int32_t readcsr(int i){
-    switch(i){
-        case 0x105:
-            return decinfo.isa.stvec;
-        case 0x142:
-            return decinfo.isa.scause;
-        case 0x100:
-            return decinfo.isa.sstatus;
-        case 0x141:
-            return decinfo.isa.sepc;
-        default:
-            assert(0 && "Unfinished readcsr");
-    }
+int32_t read_csr(int csr_num) {
+  switch (csr_num) {
+    case 0x105: return decinfo.isa.stvec;
+    case 0x142: return decinfo.isa.scause;
+    case 0x100: return decinfo.isa.sstatus;
+    case 0x141: return decinfo.isa.sepc;
+    default: panic("read_csr: invalid csr_num = %x", csr_num);
+  }
 }
 
-void writecsr(int i, int32_t val){
-    //TODO
-    switch(i){
-        case 0x105:
-            decinfo.isa.stvec = val;
-            break;
-        case 0x142:
-            decinfo.isa.scause = val;
-            break;
-        case 0x100:
-            decinfo.isa.sstatus = val;
-            break;
-        case 0x141:
-            decinfo.isa.sepc = val;
-            break;
-        default:
-            assert(0 && "Unfinished readcsr");
-    }
+int32_t write_csr(int csr_num, int32_t val) {
+  switch (csr_num) {
+    case 0x105: decinfo.isa.stvec = val; break;
+    case 0x142: decinfo.isa.scause = val; break;
+    case 0x100: decinfo.isa.sstatus = val; break;
+    case 0x141: decinfo.isa.sepc = val; break;
+    default: panic("write_csr: invalid csr_num = %x", csr_num);
+  }
+  return 0;
 }
 
 make_EHelper(syscall){
     Instr instr = decinfo.isa.instr;
-    switch(instr.funct3){
-        //ecall
-        case 0b0:
-            if((instr.val & ~(0x7f))==0){
+    switch (instr.funct3){
+    case 0x0: // ecall
+        if((instr.val & ~(0x7f))==0){
                 raise_intr(reg_l(17), decinfo.seq_pc-4);
-            }
-            else if(instr.val == 0x10200073){
-                decinfo.jmp_pc = decinfo.isa.sepc + 4;
-                rtl_j(decinfo.jmp_pc);
-            }
-            else{
-                assert(0 && "system code unfinish");
-            }
-            break;
-        // csrrw
-        case 0b001:
-            s0 = readcsr(instr.csr);
-            writecsr(instr.csr, id_src->val);
-            rtl_sr(id_dest->reg, &s0, 4);
-            break;
-        case 0b010:
-            s0 = readcsr(instr.csr);
-            writecsr(instr.csr, s0 | id_src->val);
-            rtl_sr(id_dest->reg, &s0, 4);
-            break;
-        default:
-            assert(0 && "Unfinished system op");
+        }
+        else if(instr.val == 0x10200073){
+            decinfo.jmp_pc = decinfo.isa.sepc + 4;
+            rtl_j(decinfo.jmp_pc);
+        }
+        else{
+            printf("syscall: %x\n", instr.val);
+            panic("Unfinished syscall");
+        }
+        break;
+    case 0x1: // csrrw
+        s0 = read_csr(instr.csr);
+        write_csr(instr.csr, id_src->val);
+        rtl_sr(id_dest->reg, &s0, 4);
+        break;
+    case 0x2: // csrrs
+        s0 = read_csr(instr.csr);
+        write_csr(instr.csr, s0 | id_src->val);
+        rtl_sr(id_dest->reg, &s0, 4);
+        break;
+    default:
+        panic("Unfinished syscall");
     }
 }
