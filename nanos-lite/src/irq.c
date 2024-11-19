a50875bcbb3311f20f49eeda0c2ed180c1d00e00
tracer-ics2019 <tracer@njuics.org>
2024-11-19 18:31:44 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:31:44 up  1:20,  0 users,  load average: 0.15, 0.15, 0.11 7b22a12b2effda15e80d510fc7f8f45ce260e910

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,6 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: printf("Yield event happened!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
