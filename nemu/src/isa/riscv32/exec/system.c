ae821eb2c379ff548ea4eec918f43c805572c672
tracer-ics2019 <tracer@njuics.org>
2024-11-19 18:15:10 +0800

    >  compile U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:15:10 up  1:04,  0 users,  load average: 0.22, 0.19, 0.11 f39579e2e5c4a93cb9b6773876178c5ff702877b

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -24,32 +24,30 @@ int32_t write_csr(int csr_num, int32_t val) {
 }
 
 make_EHelper(syscall){
-    Instr instr = decinfo.isa.instr;
-    switch (instr.funct3){
+  Instr instr = decinfo.isa.instr;
+  switch (instr.funct3){
     case 0x0: // ecall
-        if((instr.val & ~(0x7f))==0){
-                raise_intr(reg_l(17), decinfo.seq_pc-4);
-        }
-        else if(instr.val == 0x10200073){
-            decinfo.jmp_pc = decinfo.isa.sepc + 4;
-            rtl_j(decinfo.jmp_pc);
-        }
-        else{
-            printf("syscall: %x\n", instr.val);
-            panic("Unfinished syscall");
-        }
-        break;
-    case 0x1: // csrrw
-        s0 = read_csr(instr.csr);
-        write_csr(instr.csr, id_src->val);
-        rtl_sr(id_dest->reg, &s0, 4);
-        break;
-    case 0x2: // csrrs
-        s0 = read_csr(instr.csr);
-        write_csr(instr.csr, s0 | id_src->val);
-        rtl_sr(id_dest->reg, &s0, 4);
-        break;
-    default:
+      if((instr.val & ~(0x7f))) raise_intr(reg_l(17), decinfo.seq_pc-4);
+      else if(instr.val == 0x10200073){
+        decinfo.jmp_pc = decinfo.isa.sepc + 4;
+        rtl_j(decinfo.jmp_pc);
+      }
+      else{
+        printf("syscall: %x\n", instr.val);
         panic("Unfinished syscall");
-    }
+     }
+      break;
+    case 0x1: // csrrw
+      s0 = read_csr(instr.csr);
+      write_csr(instr.csr, id_src->val);
+      rtl_sr(id_dest->reg, &s0, 4);
+      break;
+    case 0x2: // csrrs
+      s0 = read_csr(instr.csr);
+      write_csr(instr.csr, s0 | id_src->val);
+      rtl_sr(id_dest->reg, &s0, 4);
+      break;
+    default:
+      panic("Unfinished syscall");
+  }
 }
