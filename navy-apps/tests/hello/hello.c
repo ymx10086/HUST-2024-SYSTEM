0fceba53bbee70f2ee2611e56ed87b9ad8ea08d4
tracer-ics2019 <tracer@njuics.org>
2024-11-20 18:32:06 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:32:06 up 1 day,  1:21,  0 users,  load average: 0.28, 0.30, 0.25 8c1491c68b15e85d1353fba48533c6422d08b3

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -8,7 +8,7 @@ int main() {
   while (1) {
     j ++;
     if (j == 100) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i);
+      printf("Hello World from Navy-apps for the %dth time!\n", i++);
       j = 0;
     }
   }
