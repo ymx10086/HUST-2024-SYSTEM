431979cbe35c9753d8a8678f2754ed41a9a6c209
tracer-ics2019 <tracer@njuics.org>
2024-11-20 17:23:22 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:23:22 up 1 day, 12 min,  0 users,  load average: 0.77, 0.41, 0.24 acec511297eb7d2ac19b6885aa6a80099c7276f0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -42,12 +42,12 @@ void sys_exit(int code) {
 }
 
 int sys_write(int fd, void *buf, size_t count) {
-  printf("%s\n", (char*)buf);
+  // printf("%s\n", (char*)buf);
   if(fd == 1 || fd == 2) {
     for(int i = 0; i < count; i++) {
       _putc(((char*)buf)[i]);
     }
-    printf("%d\n", count);
+    // printf("%d\n", count);
     return count;
   }
   return 0;
