157eda238e481649b5bf97c3433d17c73a8b22e7
tracer-ics2019 <tracer@njuics.org>
2024-11-20 16:55:31 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:55:31 up 23:44,  0 users,  load average: 0.42, 0.36, 0.20 fa29aa8476096c70b81f2209d8c8fcc223a760e8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,9 +11,10 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
+  
+  printf("a[0] = %d\n", a[0]);
   switch (a[0]) {
-    printf("a[0] = %d\n", a[0]);
+   
     case SYS_yield:
       printf("SYS_yield\n");
       sys_yield();
