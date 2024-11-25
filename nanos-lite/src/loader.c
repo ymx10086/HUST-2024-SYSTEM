d9849a406d04d9e7fa472e0e761ecd3a51596f2d
tracer-ics2019 <tracer@njuics.org>
2024-11-25 10:23:43 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:23:43 up 20 min,  0 users,  load average: 0.36, 0.19, 0.11 28cd15f85419f3821a7913c35bc2bb1e26db74b

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,19 +15,23 @@ extern size_t ramdisk_write(const void*, size_t, size_t);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
-  Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+  Elf_Ehdr head;
+  int fd = fs_open(filename, 0, 0);
 
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; i++) {
-    if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      memset((void*)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  fs_lseek(fd, 0, SEEK_SET);
+  fs_read(fd, &head, sizeof(head));
+
+  for (int i = 0; i < head.e_phnum; i++) {
+    Elf_Phdr temp;
+    fs_lseek(fd, head.e_phoff + i * head.e_phentsize, SEEK_SET);
+    fs_read(fd, &temp, sizeof(temp));
+    if (temp.p_type == PT_LOAD) {
+      fs_lseek(fd, temp.p_offset, SEEK_SET);
+      fs_read(fd, (void *)temp.p_vaddr, temp.p_filesz);
+      memset((void *)(temp.p_vaddr + temp.p_filesz), 0, temp.p_memsz - temp.p_filesz);
     }
   }
-  return ehdr.e_entry;
+  return head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,10 @@ void sys_yield();
 void sys_exit(int code);
 int sys_write(int fd, void *buf, size_t count);
 int sys_brk(intptr_t addr);
+int sys_open(const char *pathname, int flags, int mode);
+int sys_read(int fd, void *buf, size_t count);
+int sys_lseek(int fd, size_t offset, int whence);
+int sys_close(int fd);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -33,6 +37,18 @@ _Context* do_syscall(_Context *c) {
     case SYS_brk:
       c->GPRx = sys_brk(a[1]);
       break;
+    case SYS_open:
+      c->GPRx = sys_open((const char*)a[1], a[2], a[3]);
+      break;
+    case SYS_read:
+      c->GPRx = sys_read(a[1], (void*)(a[2]), a[3]);
+      break;
+    case SYS_lseek:
+      c->GPRx = sys_lseek(a[1], a[2], a[3]);
+      break;
+    case SYS_close:
+      c->GPRx = sys_close(a[1]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
@@ -48,16 +64,33 @@ void sys_exit(int code) {
 }
 
 int sys_write(int fd, void *buf, size_t count) {
-  if(fd == 1 || fd == 2) {
-    for(int i = 0; i < count; i++) {
-      _putc(((char*)buf)[i]);
-    }
-    return count;
-  }
-  return 0;
+  // if(fd == 1 || fd == 2) {
+  //   for(int i = 0; i < count; i++) {
+  //     _putc(((char*)buf)[i]);
+  //   }
+  //   return count;
+  // }
+  // return 0;
+  return fs_write(fd, buf, count);
 }
 
 int sys_brk(intptr_t addr) {
   program_break = addr;
   return 0;
+}
+
+int sys_open(const char *pathname, int flags, int mode) {
+  return fs_open(pathname, flags, mode);
+}
+
+int sys_read(int fd, void *buf, size_t count) {
+  return fs_read(fd, buf, count);
+}
+
+int sys_lseek(int fd, size_t offset, int whence) {
+  return fs_lseek(fd, offset, whence);
+}
+
+int sys_close(int fd) {
+  return fs_close(fd);
 }
