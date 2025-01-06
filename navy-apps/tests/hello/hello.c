aa34ea06c51170ca9195b78f2abb2b4d58742b39
tracer-ics2019 <tracer@njuics.org>
2025-01-06 20:02:53 +0800

    >  run U202115514 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:02:53 up 19:32,  1 user,  load average: 0.46, 0.14, 0.21 81516a1ea1d0b0781aa5428d928bb2a88f3cb45d

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,7 +5,7 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (1) {
+  for(int j = 0; j < 10000; j ++) {
     j ++;
     if (j == 100) {
       printf("Hello World from Navy-apps for the %dth time!\n", i++);
